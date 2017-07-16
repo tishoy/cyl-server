176f5f640f66e23a765dd8c766d68b667ea9459d
TishoY <hantishoy@sina.com>
2017-07-16 22:36:19 +0800

    登录 重置密码

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,28 @@ const router = require('koa-router')()
 
 router.prefix('/users')
 
+router.post('/login', function (ctx, next) {
+  let req = ctx.request.body
+  console.log(req.account)
+  console.log(req.session)
+  console.log(req.password)
+  if (req.account === "tishoy" && req.session === "tishoy_1_training") {
+    ctx.body = { code: 0, session: "tishoy_1_training" }
+  }
+  if (req.account === "tishoy" && req.password === "hantishoy123") {
+    ctx.body = { code: 0, session: "tishoy_1_training" }
+  }
+  ctx.body = { code: 10001 }
+})
+
+router.post('/reset', function () {
+  let req = ctx.request.body
+  console.log(ctx.request)
+  if (req.account == "tishoy") {
+
+  }
+})
+
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
@@ -9,6 +31,7 @@ router.get('/', function (ctx, next) {
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
+
 
 // router.post()
 
