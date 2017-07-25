126f2913e39529088e8889ce0e0ff7f9ad4c04e6
Tishoy Han <hantishoy@sina.com>
2017-07-25 17:52:52 +0800

    独立全部数据

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const router = require('koa-router')()
 
 const Code = require('../code')
 
+const ALL_DATA = require('./data')
+
 router.prefix('/users')
 
 /**
@@ -44,79 +46,7 @@ router.post('/login', function (ctx, next) {
   console.log(ctx.request)
   if (req.account === "tishoy" && req.password === "hantishoy123" && req.type === 1) {
     ctx.body = {
-      code: Code.LOGIC_SUCCESS, session: "tishoy_1_training", students: [
-        {
-          "base_info": {
-            "name": "tishoy",
-            "tel": "13810100010",
-            "email": "tishoy",
-            "city": "北京",
-            "level": "中级",
-            "company": "中软"
-          },
-          "personal_info": {
-            "licence": "232700198902230021",
-            "edu": "QH University",
-            "working_time": "5 year",
-            "total_amount": "",
-            "soft_amount": ""
-          },
-          "proj_exp": [
-            {
-              "id": "",
-              "name": "",
-              "time": "",
-              "actor": "",
-              "total_amount": "",
-              "soft_amount": ""
-            },
-            {
-              "id": "",
-              "name": "",
-              "time": "",
-              "actor": "",
-              "total_amount": "",
-              "soft_amount": ""
-            },
-            {
-              "id": "",
-              "name": "",
-              "time": "",
-              "actor": "",
-              "total_amount": "",
-              "soft_amount": ""
-            }
-          ],
-          // 状态 0 未进行 1 进行中 2 进行结束
-          "status": {
-            "enrolled": {
-              "status": 1,
-              "time": 1500262255
-            },
-            "arranged": {
-              "status": 1,
-              "time": 1500262255
-            },
-            "agreed": {
-              "status": 1,
-              "time": 1500262255
-            },
-            "examing": {
-              "status": 1,
-              "time": 1500262255
-            },
-            "passed": {
-              "status": 1,
-              "score": 96,
-              "time": 1500262255
-            },
-            "retry": {
-              "status": 1,
-              "time": 1500262255
-            }
-          }
-        },
-      ]
+      code: Code.LOGIC_SUCCESS, session: "tishoy_1_training", data: ALL_DATA
     }
   } else {
     ctx.body = { code: 10001, session: "", students: [] }
